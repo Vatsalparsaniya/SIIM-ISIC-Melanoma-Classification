9e32c497877f0e64050224783d65ce9a50ae1943
Vatsal Parsaniya <vatsal.pict17@sot.pdpu.ac.in>
2020-09-05 17:19:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"your@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
